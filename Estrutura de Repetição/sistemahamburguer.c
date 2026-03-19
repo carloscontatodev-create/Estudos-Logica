4093f4d62e904b01e3031615a36836d8667445fa
qva <carlos.contato.dev@gmail.com>
2026-03-19 01:47:14 -0300

    sistemahamburguer.c

--- Estrutura de Repetição/sistemahamburguer.c
+++ Estrutura de Repetição/sistemahamburguer.c
@@ -2,23 +2,39 @@
 
 int main(){
 
-    int faturamento=0, despesas=0, movimentacoes, somafa=0, somade=0;
+    int faturamento=0, despesas=0, movimentacoes, saldoFinal, decisao;
 
-    while (movimentacoes>0){
+    printf("Ola, deseja realizar a abertura do caixa (1. Sim - 2. Nao)\n");
+    scanf("%d", &decisao);
+
+    if (decisao == 1){
+
+    while (decisao=1){
 
         printf("Digite o valor: \n");
         scanf("%d", &movimentacoes);
 
         if (movimentacoes>0){
-            somafa = somafa+faturamento;
+            faturamento = faturamento+movimentacoes;
         }
         else if (movimentacoes<0){
-            somade = somade+despesas;
+            despesas = despesas+movimentacoes;
+        }
+        else if (movimentacoes == 0){
+            break;
         }
 
     }
 
-    printf ("O total faturado foi de: %d \n", somafa)
+    saldoFinal = faturamento+despesas;
+
+    printf ("O total faturado foi de: %d \n", faturamento);
+    printf ("O total de despesas foi de: %d \n", despesas);
+    printf ("Saldo final foi de: %d \n", saldoFinal);
+}
+    else{
+        printf("Ok, Obrigado! Tenha um bom dia.");
+    }
 
 
     return 0;
